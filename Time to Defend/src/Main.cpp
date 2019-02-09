a407e678753b82eab0a1304c4a8ee25f3ce703e6
Sahil Iyer <iyersahil12@gmail.com>
2019-02-09 22:40:04 +1100

    Linked SDL2 with a test initialisation.

--- Time to Defend/src/Main.cpp
+++ Time to Defend/src/Main.cpp
@@ -1,4 +1,17 @@
-int main()
-{
+#include <iostream>
 
+#include <SDL/SDL.h>
+
+
+int main(int argc, char *argv[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) != 0)
+	{
+		std::cout << "Could not initialise SDL." << std::endl;
+		return 1;
+	}
+
+	std::cin.get();
+
+	return 0;
 }
