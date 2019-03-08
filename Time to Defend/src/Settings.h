e1420989c5176f99cc34648412e48d820a0088dd
Sahil Iyer <iyersahil12@gmail.com>
2019-03-09 08:51:05 +1100

    Increased arrow speed.

--- Time to Defend/src/Settings.h
+++ Time to Defend/src/Settings.h
@@ -8,7 +8,7 @@ constexpr unsigned int NUM_OF_CELLS_Y = 27;
 constexpr unsigned int CELL_SIZE = 20;
 
 // Movement speeds
-constexpr unsigned int ARROW_SPEED = 2;
+constexpr unsigned int ARROW_SPEED = 3;
 constexpr int TOWER_ROTATION_SPEED = 3;
 constexpr unsigned int MIN_ENEMY_MOVE_TIME = 220;
 constexpr unsigned int MAX_ENEMY_MOVE_TIME = 380;
