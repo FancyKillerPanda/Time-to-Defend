c1c949a78c00228bddd98f2e21ad5f885810726d
Sahil Iyer <iyersahil12@gmail.com>
2019-03-22 18:21:58 +1100

    [BROKEN] Moved rest of common files to engine project.

--- TtD Engine/src/pch.h
+++ TtD Engine/src/pch.h
@@ -5,6 +5,11 @@
 #include <chrono>
 #include <memory>
 
+// SDL2
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+
 // Spdlog
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
